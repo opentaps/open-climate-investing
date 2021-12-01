8cc8bf826adaee7ea198f084c652c93e54e883b2
Si Chen <sichen@opensourcetrategies.com>
2021-11-30 16:29:21 -0800

    load XOP_SMOG Ortho 1 as default BMG, CARIMA as secondary

--- init_db.sh
+++ init_db.sh
@@ -16,8 +16,45 @@ CREATE TABLE _import_carbon_risk_factor (
 	PRIMARY KEY (date)
 );'
 
-psql ${DB_NAME} -c 'COPY _import_carbon_risk_factor FROM STDIN WITH (FORMAT CSV, HEADER);' < data/carbon_risk_factor.csv
+psql ${DB_NAME} -c 'COPY _import_carbon_risk_factor FROM STDIN WITH (FORMAT CSV, HEADER);' < data/bmg_xop_smog_orthogonalized_1.csv
 psql ${DB_NAME} -c "INSERT INTO carbon_risk_factor (date, bmg, factor_name) SELECT date, bmg, 'DEFAULT' FROM _import_carbon_risk_factor;"
+psql ${DB_NAME} -c 'DELETE FROM _import_carbon_risk_factor CASCADE;'
+
+psql ${DB_NAME} -c 'COPY _import_carbon_risk_factor FROM STDIN WITH (FORMAT CSV, HEADER);' < data/bmg_carima.csv
+psql ${DB_NAME} -c "INSERT INTO carbon_risk_factor (date, bmg, factor_name) SELECT date, bmg, 'CARIMA' FROM _import_carbon_risk_factor;"
+psql ${DB_NAME} -c 'DROP TABLE IF EXISTS _import_carbon_risk_factor CASCADE;'
+
+
+psql ${DB_NAME} -c 'COPY risk_free FROM STDIN WITH (FORMAT CSV, HEADER);' < data/risk_free.csv
+
+# load stocks with sector and sub-sector
+psql ${DB_NAME} -c 'COPY stocks FROM STDIN WITH (FORMAT CSV, HEADER);' < data/spx_sector_breakdown.csv
+# manual insert for the funds
+# spx
+psql ${DB_NAME} -c "INSERT INTO stocks (ticker, name) VALUES ('IVV', 'iShares S&P 500');"
+# msci
+psql ${DB_NAME} -c "INSERT INTO stocks (ticker, name) VALUES ('XWD.TO', 'iShares MSCI World');"
+
+# import components and weights of spx
+psql ${DB_NAME} -c "DROP TABLE IF EXISTS _stock_weights CASCADE; CREATE TABLE _stock_weights (
+    ticker text,
+    weight DECIMAL(5,2),
+    PRIMARY KEY (ticker)
+);
+DELETE FROM stock_components WHERE ticker = 'IVV';
+COPY _stock_weights FROM STDIN WITH (FORMAT CSV, HEADER);
+INSERT INTO stock_components (ticker, component_stock, percentage) SELECT
+'IVV', ticker, weight FROM _stock_weights;
+DROP TABLE IF EXISTS _stock_weights CASCADE;
+" < data/spx_constituent_weights.csv
+
+# import components of msci
+psql ${DB_NAME} -c "DROP TABLE IF EXISTS _stock_comps CASCADE; CREATE TABLE _stock_comps (
+    ticker text,
+    name text,
+    weight decimal(8, 5),
+psql ${DB_NAME} -c 'COPY _import_carbon_risk_factor FROM STDIN WITH (FORMAT CSV, HEADER);' < data/bmg_carima.csv
+psql ${DB_NAME} -c "INSERT INTO carbon_risk_factor (date, bmg, factor_name) SELECT date, bmg, 'CARIMA' FROM _import_carbon_risk_factor;"
 psql ${DB_NAME} -c 'DROP TABLE IF EXISTS _import_carbon_risk_factor CASCADE;'
 
 
