ca2404edec02c8c3dee62e00fc1079370fc3af9d
Jeremy Wickersheimer <jwickers@gmail.com>
2021-12-03 12:21:30 +0800

    #412 Implement the start / end date

--- bmg_series.py
+++ bmg_series.py
@@ -1,10 +1,12 @@
 
 import argparse
 import get_stocks
+import factor_regression
+import textwrap
 import pandas as pd
 
 
-def add_bmg_series(factor_name, green_ticker, brown_ticker):
+def add_bmg_series(factor_name, green_ticker, brown_ticker, start_date=None, end_date=None):
     if not factor_name:
         print(' factor name is required !')
         return False
@@ -19,6 +21,7 @@ def add_bmg_series(factor_name, green_ticker, brown_ticker):
         print(' brown ticker is required !')
         return False
 
+    print('*** adding factor {} from stocks {} and {} ...'.format(factor_name, green_ticker, brown_ticker))
     green_data = get_stocks.load_stocks_returns_from_db(green_ticker)
     print('** green_data -> ')
     print(green_data)
@@ -32,6 +35,15 @@ def add_bmg_series(factor_name, green_ticker, brown_ticker):
     merged.drop(columns=['return_x', 'return_y'], inplace=True)
     has_value = merged['bmg'].notna()
     merged = merged[has_value]
+
+    if start_date is not None:
+        start_date = factor_regression.parse_date('Start', start_date)
+        merged = merged[merged.index >= start_date]
+
+    if end_date is not None:
+        end_date = factor_regression.parse_date('End', end_date)
+        merged = merged[merged.index <= end_date]
+
     print('** merged -> ')
     print(merged)
 
@@ -48,44 +60,83 @@ def delete_bmg_series(factor_name):
         print(' DEFAULT is a reserved factor name!')
         return False
 
+    print('*** deleting factor {} ...'.format(factor_name))
     get_stocks.delete_carbon_risk_factor_from_db(factor_name)
 
     return True
 
 
-def show_bmg_series(factor_name):
+def show_bmg_series(factor_name, start_date=None, end_date=None):
     if not factor_name:
         print(' factor name is required !')
         return False
-    print(get_stocks.load_carbon_risk_factor_from_db(factor_name))
+    
+    data = get_stocks.load_carbon_risk_factor_from_db(factor_name)
+
+    if start_date is not None:
+        start_date = factor_regression.parse_date('Start', start_date)
+        data = data[data.index >= start_date]
+
+    if end_date is not None:
+        end_date = factor_regression.parse_date('End', end_date)
+        data = data[data.index <= end_date]
+
+    print(data)
+
     return True
 
 
 def main(args):
-    if args.show_factor_name:
-        return show_bmg_series(args.show_factor_name)
-    elif args.delete_factor_name:
-        return delete_bmg_series(args.delete_factor_name)
+    if not args.factor_name:
+        return False
+    if args.show:
+        return show_bmg_series(args.factor_name, start_date=args.start_date, end_date=args.end_date)
     else:
-        return add_bmg_series(args.factor_name, args.green_ticker, args.brown_ticker)
+        if args.delete:
+            if not delete_bmg_series(args.factor_name):
+                return False
+        if args.green_ticker or args.brown_ticker:
+            if not add_bmg_series(args.factor_name, args.green_ticker, args.brown_ticker, start_date=args.start_date, end_date=args.end_date):
+                return False
+        return args.delete or args.green_ticker or args.brown_ticker
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--show_factor_name",
-                        help="specify the factor name to display, for testing")
-    parser.add_argument("-d", "--delete_factor_name",
-                        help="specify the factor name to remove, cannot be DEFAULT which is a reserved name")
+    parser = argparse.ArgumentParser(
+        description="Manage BMG series",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''
+        Examples:
+
+            Delete the series for MY_FACTOR:
+                %(prog)s -n MY_FACTOR -d
+            
+            Create a series MY_FACTOR from stocks TICK1 and TICK2:
+                %(prog)s -n MY_FACTOR -g TICK1 -g TICK2
+            
+            Display the series values stored in the DB:
+                %(prog)s -o MY_FACTOR
+            
+            Create a series MY_FACTOR from stocks TICK1 and TICK2 for 
+            a given time period, deleting the existing values first:
+                %(prog)s -n MY_FACTOR -g TICK1 -g TICK2 -d -s 2000-01-01 -e 2020-12-31
+
+        ''')
+    )
     parser.add_argument("-n", "--factor_name",
-                        help="specify the factor name, cannot be DEFAULT which is a reserved name")
+                        help="specify the factor name for the BMG series to operate on, cannot be DEFAULT which is a reserved name")
+    parser.add_argument("-o", "--show", action='store_true',
+                        help="display the series from the DB, for testing")
+    parser.add_argument("-d", "--delete", action='store_true',
+                        help="remove the series from the DB")
     parser.add_argument("-g", "--green_ticker",
-                        help="specify the green ticker")
+                        help="specify the green ticker when adding a new series")
     parser.add_argument("-b", "--brown_ticker",
-                        help="specify the brown ticker")
+                        help="specify the brown ticker when adding a new series")
     parser.add_argument("-s", "--start_date",
-                        help="Sets the start date for the factor, must be in the YYYY-MM-DD format, defaults to the start date of all the data series for a given stock")
+                        help="Sets the start date for the series, must be in the YYYY-MM-DD format, defaults to the earliest date on record")
     parser.add_argument("-e", "--end_date",
-                        help="Sets the end date for the factor, must be in the YYYY-MM-DD format, defaults to the last date of all the data series for a given stock")
+                        help="Sets the end date for the series, must be in the YYYY-MM-DD format, defaults to the latest date on record")
     if not main(parser.parse_args()):
         parser.print_help()
 
