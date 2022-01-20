f33b4bf538a18e0c2f31ce25a2fda05ba11b8142
Jeremy Wickersheimer <jwickers@gmail.com>
2022-01-20 11:51:40 +0800

    #460 use db.ini for the node backend as well

--- ui/node-server/app/config/db.config.js
+++ ui/node-server/app/config/db.config.js
@@ -1,9 +1,28 @@
+const fs = require('fs');
+const ini = require('ini');
+
+let filepath = './';
+
+// lookup the config
+let depth = 1;
+while (!fs.existsSync(filepath + 'db.ini')) {
+  if (filepath.length == 2) filepath = '../';
+  else filepath = '../' + filepath;
+  depth++;
+  if (depth > 10) {
+    throw new Error('Cannot file the DB config file db.ini');
+  }
+}
+let config = ini.parse(fs.readFileSync(filepath + 'db.ini', 'utf-8'));
+console.log('Found DB config: ', filepath+'db.ini');
+
+
 module.exports = {
-  HOST: "localhost",
-  USER: "",
-  PASSWORD: "",
-  PORT: 5432,
-  DB: "open_climate_investing",
+  HOST: config.DEFAULT.databasehost,
+  USER: config.DEFAULT.databaseuser,
+  PASSWORD: config.DEFAULT.databasepassword,
+  PORT: config.DEFAULT.databaseport || 5432,
+  DB: config.DEFAULT.databasename,
   dialect: "postgres",
   pool: {
     max: 5,
