63d613302e85f4e70ae15e2cf64b25f9bdeb6afb
Jeremy Wickersheimer <jwickers@gmail.com>
2022-01-21 12:43:02 +0800

    Fix eslint missing node env

--- ui/node-server/.eslintrc.js
+++ ui/node-server/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "root": true,
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,
     },
     "extends": "eslint:recommended",
     "parserOptions": {
