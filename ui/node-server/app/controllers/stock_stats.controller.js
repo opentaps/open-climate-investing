e6c067b32479a2c58cba6c26760e6eae3de92b7d
Jeremy Wickersheimer <jwickers@gmail.com>
2022-09-13 14:50:42 +0800

    Slightly reduce the stats response size
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- ui/node-server/app/controllers/stock_stats.controller.js
+++ ui/node-server/app/controllers/stock_stats.controller.js
@@ -145,8 +145,46 @@ exports.findAll = (req, res) => {
   }
 
   const { limit, offset } = common.getPagination(page, size);
+  let attributes = [
+    "from_date",
+    "thru_date",
+    "constant",
+    "constant_std_error",
+    "constant_t_stat",
+    "constant_p_gt_abs_t",
+    "bmg",
+    "bmg_std_error",
+    "bmg_t_stat",
+    "bmg_p_gt_abs_t",
+    "mkt_rf",
+    "mkt_rf_std_error",
+    "mkt_rf_t_stat",
+    "mkt_rf_p_gt_abs_t",
+    "smb",
+    "smb_std_error",
+    "smb_t_stat",
+    "smb_p_gt_abs_t",
+    "hml",
+    "hml_std_error",
+    "hml_t_stat",
+    "hml_p_gt_abs_t",
+    "wml",
+    "wml_std_error",
+    "wml_t_stat",
+    "wml_p_gt_abs_t",
+    "jarque_bera",
+    "jarque_bera_p_gt_abs_t",
+    "breusch_pagan",
+    "breusch_pagan_p_gt_abs_t",
+    "durbin_watson",
+    "r_squared",
+  ];
+  if (!req.query["ticker"]) {
+    attributes.push("ticker");
+  }
 
   StockStat.findAndCountAll({
+    attributes,
     where: conditions ? conditions : null,
     limit,
     offset,
@@ -154,6 +192,11 @@ exports.findAll = (req, res) => {
   })
     .then((data) => {
       const response = common.getPagingData(data, page, limit);
+      response.frequency = frequency;
+      response.factor_name = factor_name;
+      if (req.query["ticker"]) {
+        response.ticker = req.query["ticker"];
+      }
       res.send(response);
     })
     .catch((err) => {
